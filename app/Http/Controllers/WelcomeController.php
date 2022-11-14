ca9ca8a094e13887f7baf7d22430e1bd95cacca0
ClaudioFoti <clau.foti@gmail.com>
2022-11-14 11:25:46 +0100

    Added admin permissions

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
         $posts = Post::all();
 
-        return view('home', compact('posts'));
+        return view('welcome', compact('posts'));
     }
 }
