77d7feb5f7cf7b337ae6e7f6a3e57c488739945c
ClaudioFoti <clau.foti@gmail.com>
2022-11-17 16:10:00 +0100

    Added time of creation in the post component

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -3,25 +3,34 @@
     <div class="p-6">
         <div class="flex items-center space-x-4">
             <div class="flex-shrink-0">
-                <img class="w-8 h-8 rounded-full" src="{{$post->author->profile?->profile_icon}}" alt="">
+                <a href="{{route('users.show',$post->author->id)}}">
+                    <img class="w-8 h-8 rounded-full" src="{{$post->author->profile?->profile_icon}}" alt="">
+                </a>
             </div>
             <div class="flex-1 min-w-0">
-                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                    {{$post->author->name}}
-                </p>
-                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                    {{$post->author->email}}
-                </p>
+                <a href="{{route('users.show',$post->author->id)}}">
+                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {{$post->author->name}}
+                    </p>
+                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                        {{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $post->created_at)->format('H:i · d.m.Y')}}
+                    </p>
+                </a>
             </div>
             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                <form method="post" action="{{$post->hasUserLiked() ? route('interactions.destroy',$post->id) : route('interactions.store')}}">
+                <form method="post"
+                      action="{{$post->hasUserLiked() ? route('interactions.destroy',$post->id) : route('interactions.store')}}">
                     @csrf
                     @if($post->hasUserLiked())
                         @method('delete')
                     @endif
                     <input type="hidden" name="post_id" value="{{$post->id}}">
                     <button type="submit">
-                        <svg class="w-6 h-6" fill="none" stroke="{{$post->hasUserLiked() ? "#0E9F6E" : "currentColor"}}" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
+                        <svg class="w-6 h-6" fill="none" stroke="{{$post->hasUserLiked() ? "#0E9F6E" : "currentColor"}}"
+                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
+                        </svg>
                     </button>
                 </form>
                 <span class="ml-1 mb-2 {{$post->hasUserLiked() ? "text-green-600" : ""}}">
@@ -32,8 +41,10 @@
         </div>
     </div>
     <div class="pl-5 pb-2">
-        <p class="mb-3 font-normal text-lg text-gray-700 dark:text-gray-400">
-            {{ $post->body }}
-        </p>
+        <a href="{{route('posts.show',$post->id)}}">
+            <p class="mb-3 font-normal text-lg text-gray-700 dark:text-gray-400">
+                {{ $post->body }}
+            </p>
+        </a>
     </div>
 </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,7 @@
 <x-site-layout title="Posts">
-
-    <div class="mb-6 flex justify-end">
-        <a href="{{route('posts.create')}}" class="p-2 bg-green-500 text-green-50 rounded-lg">Create post</a>
-    </div>
-
-    <ul class="list-disc pl-5">
+    <div class="mx-auto w-[40rem]">
         @foreach($posts as $post)
-            <li>
-                <a href="{{route('posts.show', $post->id)}}">
-                    {{ Str::limit($post->body, 80)}}
-                </a>
-            </li>
+            <x-post :post="$post" :categories="$post->categories"/>
         @endforeach
-    </ul>
-
+    </div>
 </x-site-layout>
