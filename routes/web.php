9c079942915a70b283a50c12d5c88b189027677a
ClaudioFoti <clau.foti@gmail.com>
2022-11-18 18:10:01 +0100

    Added routes to logged in user to see and create posts

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('home', \App\Http\Controllers\HomeController::class)->name('home');
     Route::resource('interactions', \App\Http\Controllers\PostUserController::class);
+    Route::resource('posts', \App\Http\Controllers\PostController::class)->only('create','show','index','store');
 });
 
 /*
@@ -27,6 +28,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 |--------------------------------------------------------------------------
 */
 Route::middleware(['auth', 'is_admin'])->group(function () {
-    Route::resource('posts', \App\Http\Controllers\PostController::class);
+    Route::resource('posts', \App\Http\Controllers\PostController::class)->except('create','show','index','store');
     Route::resource('users', \App\Http\Controllers\UserController::class);
 });
