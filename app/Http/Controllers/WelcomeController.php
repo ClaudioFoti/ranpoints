bd7f93379d0f51891b3294b0feaccd3beae6b6bf
ClaudioFoti <clau.foti@gmail.com>
2022-11-23 10:00:17 +0100

    Code pinting

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        if(auth()->id() !== null)
-        {
+        if (auth()->id() !== null) {
             return redirect(route('home'));
         }
 
