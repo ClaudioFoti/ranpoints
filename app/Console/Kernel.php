f332fbc1d38f79b35dcfd50b8211212bacfb23ee
ClaudioFoti <clau.foti@gmail.com>
2023-01-13 09:26:21 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:run')->dailyAt('4:20')->timezone('Europe/Bern');
+        $schedule->command('backup:run')->dailyAt('9:30')->timezone('Europe/Bern');
     }
 
     /**
